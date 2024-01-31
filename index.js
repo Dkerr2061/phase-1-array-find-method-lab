6de6774bf2102ce39f9052253e93353620286fa8
David Kerr <kerrky@gmail.com>
2024-01-30 23:07:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+function superbowlWin(arr) {
+  let result;
+  const found = arr.find(element => {
+    return element.result === 'W'
+  });
+  if(found) {
+    result = found['year']
+  } else if(!found) {
+    result = undefined;
+  }
+  return result;
+}
+
+
+
